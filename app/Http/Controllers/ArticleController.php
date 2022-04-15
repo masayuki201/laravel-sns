b5cb064cf21e5d5862846704fe6c4fda6f0167e0
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-04-16 02:56:10 +0900

    update:ArticleController.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Article;
-
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -13,6 +12,37 @@ class ArticleController extends Controller
     {
         $articles = Article::all()->sortByDesc('created_at');
 
-        return view('articles.index',['articles'=>$articles]);
+        return view('articles.index', ['articles' => $articles]);
     }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all());
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
+    }
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+
 }
