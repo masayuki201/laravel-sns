0ed865dbb86032fc263b54a948d66cc68381e7d5
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-04-16 23:48:03 +0900

    update:ArticleController.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
+
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
@@ -45,4 +50,8 @@ class ArticleController extends Controller
         return redirect()->route('articles.index');
     }
 
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
+    }
 }
