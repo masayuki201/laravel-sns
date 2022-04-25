a6b469648db19e69d41a4f4ab449941df34d306e
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-04-25 09:14:27 +0900

    fix:タイプミス

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike'
-import ArticleTagsInput from "./components/ArticleTagsInput";
-import FollowButton from "./components/FollowButton";
+import ArticleTagsInput from './components/ArticleTagsInput'
+import FollowButton from './components/FollowButton'
 
-const  app = new Vue({
-    el:'#app',
-    components:{
+const app = new Vue({
+    el: '#app',
+    components: {
         ArticleLike,
         ArticleTagsInput,
         FollowButton,
