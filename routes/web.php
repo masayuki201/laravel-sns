d6b1d48eac1f99ab965158b2ca250e2d796e2c86
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-04-25 23:35:12 +0900

    update:10-4 Googleへのリダイレクト処理の作成

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@
 */
 
 Auth::routes();
+Route::prefix('login')->name('login.')->group(function(){
+    Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+});
+
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
@@ -19,6 +23,7 @@ Route::prefix('articles')->name('articles.')->group(function () {
     Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
     Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
 });
+
 Route::get('/tags/{name}','TagController@show')->name('tags.show');
 
 Route::prefix('users')->name('users.')->group(function(){
