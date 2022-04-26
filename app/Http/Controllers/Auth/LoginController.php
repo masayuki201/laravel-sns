5444fa91fd3dc7171d5ec85add1b672006474614
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-04-26 23:32:14 +0900

    add:10-6 未登録ユーザーの場合にユーザー名登録画面を表示する

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -56,5 +56,10 @@ class LoginController extends Controller
             return $this->sendLoginResponse($request);
         }
 
+        return redirect()->route('register.{provider}', [
+            'provider' => $provider,
+            'email' => $providerUser->getEmail(),
+            'token' => $providerUser->token,
+        ]);
     }
 }
